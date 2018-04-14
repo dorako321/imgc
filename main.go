b3d63bb9dd35fdeda06d203e8b5673caac33b218
dorako321 <yamad@daiji.ro>
2018-04-14 14:34:02 +0900

    再帰的にファイル名を取得するようにした

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
-package imgc
+package main
+
+import (
+	"flag"
+	//"github.com/dorako321/imgc"
+	"io/ioutil"
+	"path/filepath"
+	"fmt"
+)
+
+func main(){
+	flag.Parse()
+	//var fileType = flag.String("type", "jpeg", "select file type (default is jpeg)")
+
+	// 処理対象のディレクトリを取得
+	directory := flag.Arg(0)
+	fileList := getFileList(directory);
+
+	fmt.Println(fileList)
+
+}
+
+func getFileList(dir string) []string {
+	// 対象のディレクトリを読み込み
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+	var paths []string
+	for _, file := range files {
+		if file.IsDir(){
+			paths = append(paths, getFileList(filepath.Join(dir, file.Name()))...);
+		} else {
+			paths = append(paths, filepath.Join(dir, file.Name()))
+		}
+	}
+	return paths
+}
